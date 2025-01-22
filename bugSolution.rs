3fd766c83b2bb7cb247d695627d9851c78a4da4e
BugHunter-X <askzenai@gmail.com>
2025-01-22 02:34:04 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut v = vec![1, 2, 3];
+    let first_element = v.get_mut(0);
+
+    if let Some(element) = first_element {
+        *element = 10; 
+    }
+    println!( "{:?}", v);
+}
